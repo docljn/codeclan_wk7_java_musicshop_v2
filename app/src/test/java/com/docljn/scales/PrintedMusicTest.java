05b66acce0c1d8b781e917248235f0a220831cab
DocLJN <ljnoble@gmail.com>
2018-01-20 10:59:30 +0000

    Add setters to StockItem.|

--- app/src/test/java/com/docljn/scales/PrintedMusicTest.java
+++ app/src/test/java/com/docljn/scales/PrintedMusicTest.java
@@ -24,13 +24,33 @@ public class PrintedMusicTest {
     }
 
     @Test
+    public void canSetName(){
+        musicBook.setName("Booklet");
+        assertEquals("Booklet", musicBook.getName());
+    }
+
+    @Test
     public void returnsSellingPrice(){
         assertEquals(12.99, musicBook.getSellingPrice(), 0.001);
     }
 
     @Test
+    public void canSetSellingPice(){
+        musicBook.setSellingPrice(13.99);
+        assertEquals(13.99, musicBook.getSellingPrice(), 0.001);
+    }
+
+
+
+    @Test
     public void returnsPurchasePrice(){
         assertEquals(5.00, musicBook.getPurchasePrice(), 0.001);
+    }
+
+    @Test
+    public void canSetPurchasePrice(){
+        musicBook.setPurchasePrice(3.00);
+        assertEquals(3.00, musicBook.getPurchasePrice(), 0.001);
     }
 
     @Test
