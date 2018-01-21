5e0b044c717494d863bbe2d6d7da8ee4b884d2c0
DocLJN <ljnoble@gmail.com>
2018-01-21 19:54:05 +0000

    Shop has bank balance used to purchase stock, and cash count of shop takings which can be deposited.

--- app/src/main/java/com/docljn/scales/Shop.java
+++ app/src/main/java/com/docljn/scales/Shop.java
@@ -11,11 +11,13 @@ class Shop {
     private String shopName;
     private ArrayList<StockItem> stock;
     private double cashCount;
+    private double bankBalance;
 
     public Shop(String shopName) {
         this.shopName = shopName;
         this.cashCount = 0.00;
         this.stock = new ArrayList<>();
+        this.bankBalance = 0.00;
     }
 
     public String getName() {
@@ -30,12 +32,17 @@ class Shop {
         return this.stock.size();
     }
 
+    public double getBankBalance() {
+        return this.bankBalance;
+    }
+
 
 
     public void buy(StockItem stockItem) {
-//        TODO: add a source for the money used to buy stock that isn't the cash count!
-//        At the moment, a purchase doesn't affect money anywhere!
-        this.stock.add(stockItem);
+        if(this.bankBalance >= stockItem.getPurchasePrice()){
+            this.bankBalance -= stockItem.getPurchasePrice();
+            this.stock.add(stockItem);
+        }
     }
 
     public void sell(StockItem stockItem) {
@@ -63,6 +70,16 @@ class Shop {
         }
         return maxProfit;
     }
+
+    public void invest(double investmentAmount) {
+        this.bankBalance += investmentAmount;
+    }
+
+    public void depositCash() {
+        this.bankBalance += getCashCount();
+        this.cashCount = 0.00;
+    }
+
 
 //    TODO: Remember to CAST if you want to use method specific to any of the subclasses of StockItem
 
--- app/src/test/java/com/docljn/scales/ShopTest.java
+++ app/src/test/java/com/docljn/scales/ShopTest.java
@@ -72,7 +72,34 @@ public class ShopTest {
         shop.buy(musicBook);
         shop.buy(musicBook);
         assertEquals(15.98, shop.getMaxProfit(), 0.001);
+    }
 
+    @Test
+    public void startsWithZeroBankBalance(){
+        assertEquals(0.00, shop.getBankBalance(), 0.001);
+    }
+
+    @Test
+    public void investingIncreasesBankBalance(){
+        shop.invest(1000.00);
+        assertEquals(1000.00, shop.getBankBalance(), 0.001);
+    }
+
+    @Test
+    public void buyingStockDecreasesBankBalance(){
+        shop.invest(1000);
+        shop.buy(musicBook);
+        assertEquals(995, shop.getBankBalance(), 0.001);
+    }
+
+    @Test
+    public void depositingCashClearsCashCountIncreasesBankBalance(){
+        shop.invest(1000);
+        shop.buy(musicBook);
+        shop.sell(musicBook);
+        shop.depositCash();
+        assertEquals(1007.99, shop.getBankBalance(), 0.001);
+        assertEquals(0, shop.getCashCount(), 0.001);
     }
 
 
